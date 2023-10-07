dbf83502a955c5eb87d024a991e6e716c7b4c16a
sally-AH <sallyabdalhafeez@gmail.com>
2023-10-07 21:05:13 +0300

    Frontend <> Added Laravel Project

---
+++ AdminDashboard/resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Blink Matrics Dashboard</title>
+</head>
+<body>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li><a href="">
+                    <span class="icon">
+                        <ion-icon name="logo-apple"></ion-icon>
+                    </span>
+                    <span class="title">Name</span></a>
+                </li>
+                <li><a href=""><span class="icon">XXX</span><span class="title">Name</span></a></li>
+                <li><a href=""><span class="icon">XXX</span><span class="title">Name</span></a></li>
+                <li><a href=""><span class="icon">XXX</span><span class="title">Name</span></a></li>
+
+            </ul>
+        </div>
+    </div>
+
+
+    {{-- Scripts --}}
+    <script src="assets/js/app.js"></script>
+    {{-- ICONS --}}
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
+
